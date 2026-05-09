e30f7f771d35b1bef432d3ac189c96012dd01de2
Alefe1995 <alefef4@gmail.com>
2026-05-08 23:29:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -471,7 +471,7 @@ if rc_input:
         # ATÉ 5 LINHAS = AUTO AJUSTE
         if quantidade_pedidos <= 5:
         
-            altura_pedidos = (quantidade_pedidos * 52) + 30
+            altura_pedidos = (quantidade_pedidos * 52) + 25
             scroll = False
         
         # MAIS DE 5 LINHAS = SCROLL
