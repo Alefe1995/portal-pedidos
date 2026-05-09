91bd02dd994e37c98a617562392c4955c0dc4a96
Alefe1995 <alefef4@gmail.com>
2026-05-09 17:56:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -742,7 +742,7 @@ margin-top:6px;
             """
 
             # AUTO AJUSTE
-            altura_itens = (len(itens_pedido) * 45) + 75
+            altura_itens = (len(itens_pedido) * 45) + 45
 
             components.html(
                 html_itens,
