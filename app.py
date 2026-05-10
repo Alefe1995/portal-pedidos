83a612b8bfa0e0599b86e7fd44ccb511c83253f6
Alefe1995 <alefef4@gmail.com>
2026-05-10 15:41:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -662,7 +662,7 @@ if rc_input:
 
         pedido_escolha = st.selectbox(
             "📌 Selecione um Pedido:",
-            ["123"] + pedidos_view["Pedido_Cliente"].tolist()
+            ["Selecione um Pedido"] + pedidos_view["Pedido_Cliente"].tolist()
         )
 
         # =========================
