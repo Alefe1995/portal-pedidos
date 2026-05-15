dce6a302828239b7bb7e5b395d6f25e2f9530d50
Alefe1995 <alefef4@gmail.com>
2026-05-15 12:51:47 -0300

    Update portal_backup.py

--- portal_backup.py
+++ portal_backup.py
@@ -559,11 +559,19 @@ def mostrar_portal(filtro_tipo="MASTER", filtro_valor=None):
                 .str.strip()
             )
 
-            # Mantém apenas itens dos pedidos filtrados
+            # Pega apenas pedidos cujo motivo NÃO seja Batch
+            pedidos_sem_batch = pedidos_view[
+                pedidos_view["Motivo"]
+                .astype(str)
+                .str.strip()
+                .str.lower() != "batch"
+            ]["Pedido"].astype(str).str.replace(".0", "", regex=False).str.strip()
+            
+            # Mantém apenas itens dos pedidos válidos
             ruptura_itens = itens[
-                itens["_pedido_key"].isin(pedidos_filtrados)
+                itens["_pedido_key"].isin(pedidos_sem_batch)
             ].copy()
-
+            
             # Mantém apenas itens com Status Reserva = Saldo
             ruptura_itens = ruptura_itens[
                 ruptura_itens["Status Reserva"]
